5394ecb143d6fc98c13180d8715b8d064a54cd9c
Amal Kakaiya <kakaiya858@gmail.com>
2012-03-31 05:33:16 +0100

    blogger is Threaded. BOOYA!!

--- blogger.py
+++ blogger.py
@@ -3,7 +3,10 @@ import simplejson
 import re
 from BeautifulSoup import BeautifulSoup as Soup
 from soupselect import select
+from threading import Thread, Lock
 
+dataLock = Lock()
+data = []
 
 def visible(element):
     if element.parent.name in ['style', 'script', '[document]', 'head', 'title']:
@@ -18,8 +21,37 @@ def getSearchURL(query):
 	return ('https://ajax.googleapis.com/ajax/services/search/blogs?' +
 	       'v=1.0&rsz=8&q=site:*.blogspot.com%20'+query)
 	
+def scrapeBlog(blog):
+	blogurl = blog['postUrl']
+	
+	soup = Soup(urllib2.urlopen(blogurl))
+	post = select(soup, 'div.post-body')
+	
+	title = select(soup, 'h1.title')
+	titleNoTags = Soup(str(title))
+	rawTitle = ''.join(filter(visible, titleNoTags.findAll(text=True))).strip()
+	#print rawTitle
+	
+	noScript = Soup(str(post))
+	rawText = ''.join(filter(visible, noScript.findAll(text=True))).strip()
+	#print raw_text
+	
+	blogData = {}
+	blogData['source'] = str(rawTitle)
+	blogData['title'] = blog['titleNoFormatting']
+	blogData['content'] = str(rawText)
+	blogData['date'] = blog['publishedDate']
+	blogData['url'] = str(blogurl)
+	
+	with dataLock:
+		data.append(blogData)
+	
+	
+
 	
 def getBloggerContent(query):
+	
+	global data
 	
 	url = getSearchURL(query)
 	request = urllib2.Request(url, None, {'Referer': "noreferrer"})
@@ -30,32 +62,18 @@ def getBloggerContent(query):
 
 	#print results
 	
-	data = []
+	threadPool = []
+	
+	threadCount = 0
 
 	for blog in results['responseData']['results']:
-		blogurl = blog['postUrl']
-		#print blog
+		threadPool += [Thread(target=scrapeBlog, args=(blog,)).start()]
+		threadCount += 1
+	
+	while(True):
+		if (len(data) == threadCount):
+			break
 
-		soup = Soup(urllib2.urlopen(blogurl))
-		post = select(soup, 'div.post-body')
-		
-		title = select(soup, 'h1.title')
-		titleNoTags = Soup(str(title))
-		rawTitle = ''.join(filter(visible, titleNoTags.findAll(text=True))).strip()
-		#print rawTitle
-		
-		noScript = Soup(str(post))
-		rawText = ''.join(filter(visible, noScript.findAll(text=True))).strip()
-		#print raw_text
-		
-		blogData = {}
-		blogData['source'] = str(rawTitle)
-		blogData['title'] = blog['titleNoFormatting']
-		blogData['content'] = str(rawText)
-		blogData['date'] = blog['publishedDate']
-		blogData['url'] = str(blogurl)
-		#print blogData
-		data += [blogData]
 	return data
 		
 
@@ -69,3 +87,4 @@ def getBloggerContent(query):
 		#url
 
 		#get 'postURL' entry for each post and scrape post content
+print getBloggerContent('iPhone')
